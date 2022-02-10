0e5604e1083a445a4301b4d189483c2da08d9e1a
Arjun Lalwani <arjunlalwani@Arjuns-MacBook-Pro.local>
2022-02-10 10:39:40 -0800

    update with band info...

--- lib/screens/songs_list_screen.dart
+++ lib/screens/songs_list_screen.dart
@@ -38,6 +38,7 @@ class SongsListScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            BandInfo(),
             SizedBox(
               height: 20,
             ),
@@ -55,6 +56,121 @@ class SongsListScreen extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class BandInfo extends StatelessWidget {
+  const BandInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Band",
+          style: kSectionHeadingStyle,
+        ),
+        Card(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: ExpansionTile(
+              title: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://img.particlenews.com/image.php?type=thumbnail_580x000&url=1SaRYZ_0cjJhVQr00'),
+                    radius: 30,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  // need to move this up
+                  Text("The Rock & Rolls"),
+                ],
+              ),
+              children: [
+                Text("yolo"),
+              ],
+            ),
+          ),
+        ),
+        BandFeedback(),
+      ],
+    );
+  }
+}
+
+class BandFeedback extends StatefulWidget {
+  const BandFeedback({Key? key}) : super(key: key);
+
+  @override
+  _BandFeedbackState createState() => _BandFeedbackState();
+}
+
+class _BandFeedbackState extends State<BandFeedback> {
+  int like = 0;
+  int dislike = 0;
+  var userVoted = [false, false];
+
+  updateLikes() {
+    setState(() {
+      if (userVoted[0]) {
+        --like;
+        userVoted[0] = false;
+      } else {
+        ++like;
+        userVoted[0] = true;
+      }
+      if (userVoted[1]) {
+        userVoted[1] = false;
+      }
+    });
+  }
+
+  updateDislikes() {
+    setState(() {
+      if (userVoted[1]) {
+        --dislike;
+        userVoted[1] = false;
+      } else {
+        ++dislike;
+        userVoted[1] = true;
+      }
+      if (userVoted[0]) {
+        userVoted[0] = false;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        ElevatedButton.icon(
+          onPressed: () {
+            updateLikes();
+          },
+          icon: userVoted[0]
+              ? Icon(Icons.thumb_up)
+              : Icon(Icons.thumb_up_outlined),
+          label: Text("$like"),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        ElevatedButton.icon(
+          onPressed: () {
+            updateDislikes();
+          },
+          icon: userVoted[1]
+              ? Icon(Icons.thumb_down)
+              : Icon(Icons.thumb_down_outlined),
+          label: Text("$dislike"),
+        ),
+      ],
     );
   }
 }
