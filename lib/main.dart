ac452953ef6820d8e00c6182f56f01d8700729fb
Arjun Lalwani <arjunlalwani@Arjuns-MacBook-Pro.local>
2022-02-12 20:16:53 -0800

    fixed be issues

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'controllers/requested_song_list_controller.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -20,21 +20,17 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ReqSongListController reqSongController = ReqSongListController();
-  bool reqSongListReady = false;
+  // bool reqSongListReady = true;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _createSongList();
+    // _createSongList();
   }
 
   _createSongList() async {
     // init adds songs from firebase to local copy
     await reqSongController.getSongsFromFb();
-    setState(() {
-      reqSongListReady = true;
-    });
   }
 
   @override
